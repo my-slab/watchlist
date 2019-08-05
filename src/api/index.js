9dc6712ba7bb588aea6374233c7cb80472196ffc
Mitchell Stewart <mitchell.stewart@mylsb.email>
2019-08-05 18:10:49 +1000

    :art: Add api instance with config

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import camelcaseKeys from 'camelcase-keys-deep'
+import qs from 'qs'
+import snakecaseKeys from 'snakecase-keys'
+
+const baseUrl = 'https://api.themoviedb.org/3'
+
+const instance = axios.create({
+  baseURL,
+  paramsSerializer: params =>
+    qs.stringify(snakecaseKeys(params), { arrayFormat: 'brackets' }),
+  timeout: 2000,
+  transformResponse: data => camelcaseKeys(JSON.parse(data))
+})
+
+export default instance
