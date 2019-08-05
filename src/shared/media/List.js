fe17cac0973e8e5bc02deef2d4e2c926ac5d1ec9
Mitchell Stewart <mitchell.stewart@mylsb.email>
2019-08-06 00:28:16 +1000

    :art: Add List with infinite scroll

---
+++ src/shared/media/List.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react'
+
+import { useInView } from 'react-intersection-observer'
+
+import Flex from '../../ui/Flex'
+import Card from './Card'
+import Heading from '../../ui/Heading'
+
+const MediaList = ({ media, onLoadMore, page, totalPages }) => {
+  const [ref, inView, entry] = useInView()
+  const isNotExhausted = media.length > 0 && page < totalPages
+
+  useEffect(() => {
+    inView && onLoadMore()
+  }, [inView])
+
+  return (
+    <>
+      {media.length > 0 ? (
+        <>
+          <Flex flexWrap="wrap" justifyContent="center" mb="xl">
+            {media.map(m => (
+              <Card key={m.id} {...m} />
+            ))}
+          </Flex>
+
+          {isNotExhausted ? (
+            <Flex flexWrap="wrap" justifyContent="center" mb="2xl">
+              <Heading
+                as="span"
+                bg="surface"
+                color="textSecondary"
+                fontStyle="italic"
+                fontWeight="bold"
+                mx="sm"
+                opacity={0.9}
+                p="sm"
+                ref={ref}
+              >
+                Loading more shows...
+              </Heading>
+            </Flex>
+          ) : (
+            <Flex flexWrap="wrap" justifyContent="center" mb="2xl">
+              <Heading
+                as="span"
+                bg="surface"
+                color="textSecondary"
+                fontStyle="italic"
+                fontWeight="bold"
+                mx="sm"
+                opacity={0.9}
+                p="sm"
+              >
+                No more shows
+              </Heading>
+            </Flex>
+          )}
+        </>
+      ) : (
+        <div>no results</div>
+      )}
+    </>
+  )
+}
+
+MediaList.displayName = 'MediaList'
+export default MediaList
