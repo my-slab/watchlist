9d95b079da840e3a761aa71f3c99a601b7076428
Mitchell Stewart <mitchell.stewart@mylsb.email>
2019-08-07 21:29:03 +1000

    :bug: Fixup axios config, add transforms

--- src/shared/api.js
+++ src/shared/api.js
@@ -3,22 +3,39 @@ import camelcaseKeys from 'camelcase-keys-deep'
 import qs from 'qs'
 import snakecaseKeys from 'snakecase-keys'
 
-const baseUrl = 'https://api.themoviedb.org/3'
-const apiKey = process.env.API_KEY
+import { API_KEY } from './config'
+
+const baseURL = 'https://api.themoviedb.org/3'
+const apiKey = API_KEY
+
+export const pickData = ({ data }) => data
 
 const instance = axios.create({
-  baseUrl,
-  paramsSerializer: params => {
-    const baseParams = {
-      apiKey
-    }
+  baseURL,
 
-    return qs.stringify(snakecaseKeys({ ...baseParams, ...params }), {
+  params: {
+    apiKey
+  },
+
+  headers: {
+    'content-type': 'application/json'
+  },
+
+  transformRequest: [
+    data => data && JSON.stringify(snakecaseKeys(data)),
+    ...axios.defaults.transformRequest
+  ],
+
+  paramsSerializer: params => {
+    const result = qs.stringify(snakecaseKeys(params), {
       arrayFormat: 'brackets'
     })
+
+    return result
   },
-  timeout: 2000,
-  transformResponse: data => camelcaseKeys(JSON.parse(data))
+
+  timeout: 10000,
+  transformResponse: [data => camelcaseKeys(JSON.parse(data))]
 })
 
 export default instance
