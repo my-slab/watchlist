9a23292246178cfd8da0e996a0c072c60615b883
Mitchell Stewart <mitchell.stewart@mylsb.email>
2019-08-08 11:18:00 +1000

    :art: Abstract api/results into hook

---
+++ src/shared/hooks/useGetWithPagedResults.js
@@ -0,0 +1,53 @@
+import { useState, useRef } from 'react'
+
+import isEqual from 'lodash.isequal'
+import merge from 'lodash.merge'
+import useDeepCompareEffect from 'use-deep-compare-effect'
+
+import api, { pickData } from '../api'
+import useLoading from './useLoading'
+
+// super leaky abstraction, but it saves us a bunch of code for now.
+export default function useGetWithPagedResults(url, config = {}) {
+  const initialState = { results: [], page: 1, totalPage: undefined }
+  const [{ page, results, totalPages }, setResults] = useState(initialState)
+  const { pop, push } = useLoading()
+  const oldDeps = useRef()
+
+  const handleLoadMore = () =>
+    page < totalPages && setResults({ results, totalPages, page: page + 1 })
+
+  const concat = ({ results: r, ...pageInfo }) => {
+    setResults({ results: [...results, ...r], ...pageInfo })
+  }
+
+  useDeepCompareEffect(() => {
+    let setStrategy = setResults
+    let p = page
+
+    if (oldDeps.current) {
+      const [oldPage, oldConfig] = oldDeps.current
+
+      if (isEqual(oldConfig, config)) {
+        setStrategy = concat
+      }
+
+      if (oldPage === page) {
+        p = 1
+      }
+    }
+
+    push()
+
+    api
+      .get(url, merge({ params: { page: p } }, config))
+      .then(pickData)
+      .then(setStrategy)
+      .finally(pop)
+      .finally(() => {
+        oldDeps.current = [page, config]
+      })
+  }, [page, config])
+
+  return [{ page, results, totalPages }, handleLoadMore]
+}
